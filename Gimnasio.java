60e20ffb96df3b445e2aee46198dd517e8db1256
Cristina Afonso <chris.afonso.otero@gmail.com>
2025-01-21 17:26:55 +0000

    Update Gimnasio.java

--- Gimnasio.java
+++ Gimnasio.java
@@ -1,24 +1,26 @@
 public class Gimnasio {
     private String nombre;
+    private String ciudad;
     private String[] actividades = {"Yoga", "Boxeo", "Funcional"};
 
-    public Gimnasio(String nombre) {
+    public Gimnasio(String nombre, String ciudad) {
         this.nombre = nombre;
+        this.ciudad = ciudad;
     }
 
     public void mostrarActividades() {
-        System.out.println("Actividades disponibles en el gimnasio " + nombre + ":");
+        System.out.println("Actividades disponibles en el gimnasio " + nombre + " en " + ciudad + ":");
         for (String actividad : actividades) {
             System.out.println("- " + actividad);
         }
     }
 
     public void registrarUsuario(String usuario) {
-        System.out.println("Usuario " + usuario + " registrado en el gimnasio " + nombre + ".");
+        System.out.println("Usuario " + usuario + " registrado en el gimnasio " + nombre + " en " + ciudad + ".");
     }
 
     public static void main(String[] args) {
-        Gimnasio miGimnasio = new Gimnasio("FitLife Gym");
+        Gimnasio miGimnasio = new Gimnasio("FitLife Gym", "Santa Cruz de Tenerife");
         miGimnasio.mostrarActividades();
         miGimnasio.registrarUsuario("Juan Perez");
     }
